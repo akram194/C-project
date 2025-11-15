65968608a7294367f216221ed8ca515826e591a1
Cloud User <ec2-user@ip-172-31-8-136.ap-south-1.compute.internal>
2025-11-15 18:06:50 +0000

    added c project file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void biggest2();     // function from big2.c
+void factorial();    // function from fact.c
+void pallindrom();
+int main() {
+    biggest2();
+    factorial();
+    pallindrom();
+    return 0;
+}
+
